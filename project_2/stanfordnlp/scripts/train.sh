addf95f9dd23f26f74afddc05f5d599fb779cfa9
jackintaiwan <jackconvolution@gmail.com>
2019-05-06 23:23:01 +0800

    update: train.sh

--- project_2/stanfordnlp/scripts/train.sh
+++ project_2/stanfordnlp/scripts/train.sh
@@ -1,1 +1,3 @@
-bash scripts/run_depparse.sh Chinese gold --batch_size 1000
+./scripts/run_depparse.sh Chinese gold --batch_size 1000
+./scripts/run_depparse.sh Chinese gold --batch_size 1000 --tag_emb_dim 150 --word_emb_dim 150 --hidden_dim 600
+./scripts/run_depparse.sh Chinese gold --batch_size 1000 --tag_emb_dim 200 --word_emb_dim 150 --no_char --lr 0.001
